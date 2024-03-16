160022620bbba55e816b41c2156b55cb72004f57
Burcu Mengu <burcumengu@gmail.com>
2024-03-16 23:00:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        
+        // this.style.color = "white";  tıkladığın beyaz renk olur
+
+    });
+}
+
+
+
+
+
+//        var audio = new Audio("sounds/tom-1.mp3");
+//        audio.play();
